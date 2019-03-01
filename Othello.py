81eaba48a8fa7cc82e946d51115a5a66fc1cab2e
DehNutCase <dslayerz@hotmail.com>
2019-03-01 13:53:42 -0800

    Started work on gameboard---it has 64 tiles

--- Othello.py
+++ Othello.py
@@ -14,3 +14,55 @@ new piece and any old same colored pieces are flipped to the same color.
 
 Scoring is based on the number of pieces on the board, 1 point per piece of your color.
 """
+
+import graphics
+
+"""First step is to create the gameboard. First idea to do so is to create 64 'tile' objects
+which look like squares on the screen and contain information on whether they are 'black,' 'white,'
+or 'empty.'"""
+
+"""First idea seems rather infesible, instead trying to create a 'board' object which stores the information
+of tiles inside a 2D array"""
+
+
+screen = graphics.GraphWin("Othello Python Game", 800, 800)
+screen.update()
+
+class Board(graphics.Rectangle):
+    def __init__(self, p1, p2):
+        graphics.Rectangle.__init__(self, p1, p2)
+        self.tiles = [[]]
+        for i in range(8):
+            self.tiles.append([])
+            for j in range(8):
+                self.tiles[i].append('')
+
+        self.clear_board()
+        self.setFill('white')
+        self.setOutline('black')
+        self.setWidth(5)
+        
+
+    def clear_board(self):
+        for i in range(8):
+            for j in range(8):
+                self.tiles[i][j] = 'empty'
+
+        self.tiles[3][3] = 'white'
+        self.tiles[4][3] = 'black'
+        self.tiles[3][4] = 'black'
+        self.tiles[4][4] = 'white'
+
+
+gameboard = Board(graphics.Point(80,720), graphics.Point(720,80))
+gameboard.draw(screen)
+
+for i in range(8):
+    aLine = graphics.Line(graphics.Point((i+1)*80,720), graphics.Point((i+1)*80, 80))
+    aLine.setWidth(4)
+    aLine.setFill('black')
+    aLine.draw(screen)
+    aLine = graphics.Line(graphics.Point(80,(i+1)*80), graphics.Point(720,(i+1)*80))
+    aLine.setWidth(4)
+    aLine.setFill('black')
+    aLine.draw(screen)
