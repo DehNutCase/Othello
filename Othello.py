9f22b0a415a9b9628b48a4c8a4a6aef71488d957
DehNutCase <dslayerz@hotmail.com>
2019-03-01 16:26:43 -0800

    Added ability to flip pieces while placing

--- Othello.py
+++ Othello.py
@@ -46,6 +46,7 @@ class Board(graphics.Rectangle):
         self.setOutline('black')
         self.setWidth(5)
         self.draw(screen)
+        self.draw_grid()
 
     def clear_board(self):
         for i in range(8):
@@ -69,10 +70,56 @@ class Board(graphics.Rectangle):
             aLine.setFill('black')
             aLine.draw(screen)
 
+    def is_on_board(self, x, y):
+        if x < 0 or x > 7 or y < 0 or y > 7:
+            return False
+        else:
+            return True
+
+    def place_black(self, x, y):
+        """Place a black piece, if called on a legal location, first places a black piece on the location,
+        then flips the white pieces lying on a straight line between that piece and another black piece
+        before passing the turn. Does nothing otherwise."""
+        
+        if not self.is_on_board(x,y) or gameboard.tiles[x][y] != 'empty':
+            return
+        to_flip = []
+        for dx, dy in [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]:
+            temp_x, temp_y = x, y
+            temp_x += dx
+            temp_y += dy
+            if self.is_on_board(temp_x,temp_y) and gameboard.tiles[temp_x][temp_y] == 'white':
+                temp_x += dx
+                temp_y += dy
+                if not self.is_on_board(temp_x,temp_y):
+                    continue
+                while gameboard.tiles[temp_x][temp_y] == 'white':
+                    temp_x += dx
+                    temp_y += dy
+                    if not self.is_on_board(temp_x,temp_y):
+                        break
+                if not self.is_on_board(temp_x, temp_y):
+                    continue
+                if gameboard.tiles[temp_x][temp_y] == 'black':
+                    while True:
+                        temp_x -= dx
+                        temp_y -= dy
+                        if temp_x == x and temp_y == y:
+                            break
+                        to_flip.append([temp_x, temp_y])
+        if not to_flip:
+            return
+
+        gameboard.tiles[x][y] = 'black'
+        for x,y in to_flip:
+            gameboard.tiles[x][y] = 'black'
+        update_tiles()
+        return
+                    
+
 
 
 gameboard = Board(graphics.Point(80,720), graphics.Point(720,80))
-gameboard.draw_grid()
 
 #creates a 2D list of every single tile
 tiles = [[graphics.Circle(graphics.Point(80 * (i + 1) + 40, 80 * (j + 1) + 40), 25) for j in range(8)] for i in range(8)]
@@ -88,13 +135,15 @@ def update_tiles():
                     tiles[i][j].setWidth(2)
                     tiles[i][j].setFill('black')
                     tiles[i][j].draw(screen)
+            else:
+                if gameboard.tiles[i][j] == 'white':
+                    tiles[i][j].setWidth(2)
+                if gameboard.tiles[i][j] == 'black':
+                    tiles[i][j].setWidth(2)
+                    tiles[i][j].setFill('black')
             if gameboard.tiles[i][j] == 'empty':
                 tiles[i][j].undraw()
 
-update_tiles()
 
-gameboard.tiles[1][1] = 'black'
-update_tiles()
-
-gameboard.tiles[1][1] = 'empty'
+gameboard.place_black(3,2)
 update_tiles()
